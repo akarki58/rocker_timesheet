4a66c1683c6207e37d3ad6cc6c8146fc3db9374e
Antti <antti.karki@iki.fi>
2021-04-23 09:47:22 +0300

    1314

--- rocker_timesheet/__manifest__.py
+++ rocker_timesheet/__manifest__.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
 #
+#############################################################################
+#
+#    Copyright (C) 2021-Antti Kärki.
+#    Author: Antti Kärki.
+#    email: antti.rocker.karki@outlook.com
+
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
 
 
 {
     'name': 'Rocker Timesheet',
-    'summary': 'ht_timesheet supercharged',
+    'summary': 'hr_timesheet supercharged',
     'description': 'Probably most fastes way to report work done',
     'author': 'Antti Kärki',
     'license': 'AGPL-3',
