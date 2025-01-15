58e37f350656aca007710b08e441adc1cdd052a6
Akarki58 <antti.karki@iki.fi>
2025-01-15 16:24:58 +0200

    third

---
+++ rocker_timesheet/static/src/views/list/list_controller.js
@@ -0,0 +1,41 @@
+/** @odoo-module */
+//import { registry } from '@web/core/registry';
+//import { listView } from '@web/views/list/list_view';
+import { ListController } from '@web/views/list/list_controller';
+import { useService } from "@web/core/utils/hooks";
+
+export class RockerListController extends ListController {
+    setup() {
+        console.log('setup');
+        super.setup();
+        this.actionService = useService("action");
+      }
+    async OnRollerClick() {
+        var self = this;
+        await this.actionService.doAction("rocker_timesheet.action_create_rolling", {
+            on_close: function () {
+                console.log('OnRollerClick close');
+            }
+        });
+        $(document).find('.o_list_button_add').click();
+    }
+    async all() {await this.actionService.doAction("rocker_timesheet.action_searchpanel_all_tasks", {});}
+    async member() {await this.actionService.doAction("rocker_timesheet.action_searchpanel_member_tasks", {});}
+    async billable() {await this.actionService.doAction("rocker_timesheet.action_searchpanel_billable_tasks", {});}
+    async nonbillable() {await this.actionService.doAction("rocker_timesheet.action_searchpanel_nonbillable_tasks", {});}
+    async internal() {await this.actionService.doAction("rocker_timesheet.action_searchpanel_internal_tasks", {});}
+    async mine() {await this.actionService.doAction("rocker_timesheet.action_searchpanel_mine_tasks", {});}
+
+}
+RockerListController.template = "rocker_timesheet.RockerListController";
+//registry.category("views").add("rocker_list", {
+//    ...listView,
+//    Controller: RockerListController,
+//    buttonTemplate: "rocker_timesheet.list_buttons",
+//});
+
+RockerListController.components = {
+    ...RockerListController.components,
+
+//    QuickCreateFormView: CalendarQuickCreate,
+}
---
+++ rocker_timesheet/static/src/views/list/list_view.js
@@ -0,0 +1,14 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { listView } from "@web/views/list/list_view";
+import { RockerListController } from './list_controller';
+
+export const RockerListView = {
+    ...listView,
+    Controller: RockerListController,
+    buttonTemplate: "rocker_timesheet.list_buttons",
+};
+
+registry.category("views").add("rocker_list", RockerListView);
+
